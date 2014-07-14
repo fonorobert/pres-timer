9909ef7682603cac615939f4a612bf7f78805441
fonorobert <fonorobert@gmail.com>
2014-07-14 20:57:14 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+  
+  concat: {
+    options: {
+      stripBanners: true,
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> compiled on ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */',
+    },
+    dev: {
+      src: ['src/js/*.js'],
+      dest: 'js/main.js',
+    },
+  },
+
+  uglify: {
+    prod: {
+      files: {
+        'js/main.min.js': ['src/js/*.js'],
+      }
+    }
+  },
+
+  jshint: {
+    dev: ['src/js/*.js'],
+    gruntfile: ['Gruntfile.js'],
+  },
+
+  less: {
+    dev: {
+      files: {
+        "css/main.css": "src/less/*.less",
+      }
+    },
+    prod: {
+      options: {
+        cleancss: true,
+      },
+      files: {
+        "css/main.min.css": "src/less/*.less",
+      },
+    },
+  },
+
+  watch: {
+    js: {
+      files: ['src/js/*.js'],
+      tasks: ['jshint:dev', 'concat:dev'],
+    },
+    less: {
+      files: ['src/less/*.less'],
+      tasks: ['less:dev'],
+    },
+    gruntfile: {
+      files: ['Gruntfile.js'],
+      tasks: ['jshint:gruntfile'],
+    },
+  },
+
+
+
+});
+
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-concat');
+
+
+grunt.registerTask('dev', ['jshint:dev', 'concat:dev', 'less:dev']);
+grunt.registerTask('prof', ['uglify:prod', 'less:prod']);
+
+
+};
